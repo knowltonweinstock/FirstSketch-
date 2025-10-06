192f9087ed4e05988e591142f66caff8df2bd18f
knowltonweinstock <kweinstock@tulane.edu>
2025-10-06 13:54:04 -0500

    Add files via upload

--- interactiveGame.js
+++ interactiveGame.js
@@ -3,39 +3,48 @@
 //candy corn variables
 let candyCornXpos = 0
 let candyCornYpos = 0
-let candyCornYSpeed = -5
+let candyCornYSpeed = -3
 let candyCornDiameter = 100
 let candyCorn //image variable for candy corn
 
-//pink candy variable
-let pinkCandyXpos = 0
-let pinkCandyYpos = 0
-let pinkCandyYSpeed = -5
-let pinkCandyDiameter = 100
-let pinkCandy //image variable for candy corn
+//santa candy variable
+let santaCandyXpos = 0
+let santaCandyYpos = 0
+let santaCandyYSpeed = -4
+let santaCandyDiameter = 100
+let santaCandy //image variable for candy corn
 
 
-//purple candy variables
-let purpleCandyXpos = 0
-let purpleCandyYpos = 0
-let purpleCandyYSpeed = -5
-let purpleCandyDiameter = 100
-let purpleandy //image variable for candy corn
+//happy candy variables
+let happyCandyXpos = 0
+let happyCandyYpos = 0
+let happyCandyYSpeed = -5
+let happyCandyDiameter = 100
+let happyCandy //image variable for candy corn
 
+
+//firework variables
+let fireworks 
+
+//time variables
+let timer = 35
 
 
 let startGameBool = true
 
 let winScreenBool = false
 
+let loseGameBool = false
+
 let score= 0 
 
 
 function preload(){
 	candyCorn = loadImage('halloweenCandy.png')
-	pinkCandy = loadImage('pinkCandy.jpg')
-	purpleCandy = loadImage('purpleCandy.jpg')
+	santaCandy = loadImage('santaCandy.png')
+	happyCandy = loadImage('happyCandy.png')
 	candyLand = loadImage('candyLand.jpg')
+	fireworks = loadImage('giphy.gif')
 }
 
 
@@ -45,6 +54,7 @@ function setup(){
 	yPos = windowHeight + 50
 
 	imageMode(CENTER)
+	//rectMode(CENTER)
 
 }
 
@@ -58,18 +68,28 @@ function draw(){
 	if(winScreenBool == true){
 		winGame()
 	}
+
+	if(loseGameBool == true){
+		loseGame()
+	}
 }
 
 
 function game(){
-
-	background('candyland.jpg')
-	candyLand = loadImage('candyLand.jpg')
+	textAlign(LEFT)
+	//background('candyland.jpg')
+	image(candyLand, windowWidth/2, windowHeight/2, windowWidth, windowHeight)
 	textSize(30)
 
-	text(score, 10, 100)
+	fill(255)
+	noStroke()
+	rect(45, 30, 300, 80)
+	fill(0)
+	text('Collect 30 Candies!', 50, 70)
+	text(score, 50, 100)
+	text(timer, 50, 140)
 	
-	//image(backgroundImage, windowWidth/2, windowHeight/2, windowWidth, windowHeight)
+	//
 	
 	//candy corn game mechanics
 
@@ -79,14 +99,14 @@ function game(){
 
 	if(dist(mouseX, mouseY, candyCornXpos, candyCornYpos) < candyCornDiameter/2 && mouseIsPressed){
 		//score = score + 5
-		score + 2
+		score++
 		candyCornYSpeed = candyCornYSpeed * 1.2
 		candyCornXpos = random(50, windowWidth -50)
 		candyCornYpos = windowHeight + 50
 	}
 
 	if(candyCornYpos <=0){
-	
+
 		candyCornYSpeed = candyCornYSpeed * 1.2
 		print("minus 1")
 		candyCornXpos = random(50, windowWidth -50)
@@ -100,76 +120,148 @@ function game(){
 	}
 
 
-	//pink candy game mechanics
+	//santa candy game mechanics
 
-	image(pinkCandy, pinkCandyXpos, pinkCandyYpos, pinkCandyDiameter, pinkCandyDiameter)
+	image(santaCandy, santaCandyXpos, santaCandyYpos, santaCandyDiameter, santaCandyDiameter)
 
-	pinkCandyYpos = pinkCandyYpos + pinkCandyYSpeed
+	//image(santaCandy, windowWidth/2, windowHeight/2)
 
-	if(dist(mouseX, mouseY, pinkCandyXpos, pinkCandyYpos) < pinkCandyDiameter/2 && mouseIsPressed){
+	santaCandyYpos = santaCandyYpos + santaCandyYSpeed
+
+	if(dist(mouseX, mouseY, santaCandyXpos, santaCandyYpos) < santaCandyDiameter/2 && mouseIsPressed){
 		//score = score + 5
-		score + 5
-		pinkCandyYSpeed = pinkCandyYSpeed * 1.2
-		pinkCandyXpos = random(50, windowWidth -50)
-		pinkCandyYpos = windowHeight + 50
+		score = score + 5
+		santaCandyYSpeed = santaCandyYSpeed * random(1.2, 1.5)
+		santaCandyXpos = random(50, windowWidth -50)
+		santaCandyYpos = windowHeight + 50
 	}
 
-	if(pinkCandyYpos <=0){
-	
-		pinkCandyYSpeed = pinkCandyYSpeed * 1.2
+	if(santaCandyYpos <=0){
+
+		santaCandyYSpeed = santaCandyYSpeed * 1.2
 		print("minus 1")
-		pinkCandyXpos = random(50, windowWidth -50)
-		pinkCandyYpos = windowHeight + 50
+		santaCandyXpos = random(50, windowWidth -50)
+		santaCandyYpos = windowHeight + 50
 
-		print(pinkCandyYSpeed)
+		print(santaCandyYSpeed)
 	}
 
-	if(pinkCandyYSpeed <= -17){
-		pinkCandyYSpeed = -17
+	if(santaCandyYSpeed <= -17){
+		santaCandyYSpeed = -17
 	}
 
 
 
 
-	//purple candy game mechanics
+	//happy candy game mechanics
 
-	image(purpleCandy, purpleCandyXpos, purpleCandyYpos, purpleCandyDiameter, purpleCandyDiameter)
+	image(happyCandy, happyCandyXpos, happyCandyYpos, happyCandyDiameter, happyCandyDiameter)
 
-	purpleCandyYpos = purpleCandyYpos + purpleCandyYSpeed
+	happyCandyYpos = happyCandyYpos + happyCandyYSpeed
 
-	if(dist(mouseX, mouseY, purpleCandyXpos, purpleCandyYpos) < purpleCandyDiameter/2 && mouseIsPressed){
+	if(dist(mouseX, mouseY, happyCandyXpos, happyCandyYpos) < happyCandyDiameter/2 && mouseIsPressed){
 		//score = score + 5
-		score++
-		purpleCandyYSpeed = purpleCandyYSpeed * 1.2
-		purpleCandyXpos = random(50, windowWidth -50)
-		purpleCandyYpos = windowHeight + 50
+		score = score + 3
+		happyCandyYSpeed = happyCandyYSpeed * random(1.2, 1.5)
+		happyCandyXpos = random(50, windowWidth -50)
+		happyCandyYpos = windowHeight + 50
 	}
 
-	if(purpleCandyYpos <=0){
-	
-		purpleCandyYSpeed = purpleCandyYSpeed * 1.2
+	if(happyCandyYpos <=0){
+
+		happyCandyYSpeed = happyCandyYSpeed * random(1.2, 1.5)
 		print("minus 1")
-		purpleCandyXpos = random(50, windowWidth -50)
-		purpleCandyYpos = windowHeight + 50
+		happyCandyXpos = random(50, windowWidth -50)
+		happyCandyYpos = windowHeight + 50
 
-		print(purpleCandyYSpeed)
+		print(happyCandyYSpeed)
 	}
 
-	if(purpleCandyYSpeed <= -17){
-		purpleCandyYSpeed = -17
+	if(happyCandyYSpeed <= -17){
+		happyCandyYSpeed = -17
 	}
 
 
 
-	if(score == 30){
+	if(score >= 30){
 		winScreenBool = true
 		startGameBool = false
+
+	}
+
+	//timer function
+
+	if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+		timer --;
+	}
+
+	if (timer == 0) {
+
+		startGameBool = false
+		loseGameBool = true
+
+
 	}
 }
 
 
 function winGame(){
+	//background('fireworks.gif')
+	image(fireworks, windowWidth/2, windowHeight/2, windowWidth, windowHeight)
+	textAlign(CENTER)
+	fill(255)
+	text('YOU WIN! Press R to Reload Game', windowWidth/2, windowHeight/2)
 
+
+
+}
+
+function loseGame(){
+	background(0)
+	textAlign(CENTER)
+	fill(255)
+	text("GAME OVER", windowWidth/2, windowHeight/2);
+}
+
+
+
+function keyPressed(){
+
+	if(winScreenBool == true){
+		if(key === 'r'){
+			winScreenBool = false
+			startGameBool = true
+			loseGameBool = false 
+
+			score = 0
+			candyCornYSpeed = -3
+			santaCandyYSpeed = -4
+			happyCandyYSpeed = -5
+
+			timer = 35
+
+
+		}
+
+	}
+
+	if(loseGameBool == true){
+		if(key === 'r'){
+			winScreenBool = false
+			startGameBool = true
+			loseGameBool = false 
+
+			score = 0
+			candyCornYSpeed = -3
+			santaCandyYSpeed = -4
+			happyCandyYSpeed = -5
+
+			timer = 35
+
+
+		}
+
+	}
 
 
 }
